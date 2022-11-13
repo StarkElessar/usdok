bbfc4597bee9e9fd0187dc26adb289a38a14bcdf
Stark <serj.elessar@gmail.com>
2022-11-13 13:20:55 +0300

    fix: добавил проверку, чтобы на других страницах не было ошибки

--- src/js/helpers/dropdownGroup.js
+++ src/js/helpers/dropdownGroup.js
@@ -1,36 +1,39 @@
 export const dropdownGroup = () => {
   const dropdownWrapper = document.querySelector('.custom-dropdown')
-  const dropdownButtonSelect = document.getElementById('button-dropdown')
-  const dropdownButtonLabel = dropdownButtonSelect.querySelector(
-    '.custom-dropdown__label'
-  )
-  const dropdownList = document.getElementById('dropdown-list')
-  const dropdownInput = document.getElementById('dropdown-input')
 
   if (dropdownWrapper) {
-    document.addEventListener('click', (event) => {
-      const target = event.target
+    const dropdownButtonSelect = document.getElementById('button-dropdown')
+    const dropdownButtonLabel = dropdownButtonSelect.querySelector(
+      '.custom-dropdown__label'
+    )
+    const dropdownList = document.getElementById('dropdown-list')
+    const dropdownInput = document.getElementById('dropdown-input')
 
-      if (target !== dropdownButtonSelect) {
-        dropdownButtonSelect.classList.remove('_active')
-        dropdownList.classList.remove('_show')
-      }
-      if (target && target.closest('.custom-dropdown__btn')) {
-        dropdownButtonSelect.classList.toggle('_active')
-        dropdownList.classList.toggle('_show')
-      }
-      if (target.hasAttribute('data-value')) {
-        dropdownButtonLabel.innerText = target.innerText
-        dropdownInput.value = target.dataset.value
-        dropdownButtonSelect.focus()
-      }
-    })
+    if (dropdownWrapper) {
+      document.addEventListener('click', (event) => {
+        const target = event.target
 
-    dropdownWrapper.addEventListener('keydown', (event) => {
-      if (event.key === 'Tab' || event.key === 'Escape') {
-        dropdownButtonSelect.classList.remove('_active')
-        dropdownList.classList.remove('_show')
-      }
-    })
+        if (target !== dropdownButtonSelect) {
+          dropdownButtonSelect.classList.remove('_active')
+          dropdownList.classList.remove('_show')
+        }
+        if (target && target.closest('.custom-dropdown__btn')) {
+          dropdownButtonSelect.classList.toggle('_active')
+          dropdownList.classList.toggle('_show')
+        }
+        if (target.hasAttribute('data-value')) {
+          dropdownButtonLabel.innerText = target.innerText
+          dropdownInput.value = target.dataset.value
+          dropdownButtonSelect.focus()
+        }
+      })
+
+      dropdownWrapper.addEventListener('keydown', (event) => {
+        if (event.key === 'Tab' || event.key === 'Escape') {
+          dropdownButtonSelect.classList.remove('_active')
+          dropdownList.classList.remove('_show')
+        }
+      })
+    }
   }
 }
