b3437c6652df906fb339a56344e90bd3753573a2
Stark <serj.elessar@gmail.com>
2022-11-22 00:51:44 +0300

    feat: settings page logic js

---
+++ src/js/modules/settingsPage.js
@@ -0,0 +1,46 @@
+import { settingsAccordion } from "./settingsAccordion"
+
+const settingsPage = () => {
+  const settingsContainer = document.getElementById('settingsTable')
+  
+  if (settingsContainer) {
+    const settingsMenuButtons = Array.from(settingsContainer.querySelectorAll('[data-menu-settings]'))
+    const settingsContentItems = Array.from(settingsContainer.querySelectorAll('.settings-accordion__body'))
+    const settingsList = Array.from(settingsContainer.querySelectorAll('[data-type-settings]'))
+
+    settingsAccordion()
+
+    window.addEventListener('resize', () => {
+      settingsContentItems.forEach((item) => item.style.height = null)
+      settingsList.forEach((itemLi) => {
+        itemLi.classList.remove('_active')
+        itemLi.classList.remove('_is-open')
+      })
+      
+      settingsAccordion()
+      if (window.innerWidth > 768) {
+        settingsList[0].classList.add('_active')
+      } else {
+        settingsList[0].classList.add('_is-open')
+
+        const contentFirstItem = settingsList[0].querySelector('.settings-accordion__body')
+        contentFirstItem.style.height = `${contentFirstItem.scrollHeight}px`
+      } 
+    })
+
+    settingsMenuButtons.forEach((button) => {
+      button.addEventListener('click',  () => {
+        const settingId = button.dataset.menuSettings
+        const settingCurrentItem = settingsContainer.querySelector(`[data-type-settings="${settingId}"]`)
+
+        settingsMenuButtons.forEach((button) => button.classList.remove('_active'))
+        settingsList.forEach((item) => item.classList.remove('_active'))
+
+        button.classList.add('_active')
+        settingCurrentItem.classList.add('_active')
+      })
+    })
+  }
+}
+
+export { settingsPage }
