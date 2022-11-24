1f868e39afb9152848355928aca16b1f3dad82a3
Stark <serj.elessar@gmail.com>
2022-11-24 21:26:39 +0300

    fix: добавил класс для определения регистрация или авторизация

--- src/js/modules/toggleWindowAuth.js
+++ src/js/modules/toggleWindowAuth.js
@@ -12,10 +12,10 @@ export const toggleWindowAuth = () => {
       console.log('click')
       windowAuth.classList.add('_show')
       bodyLock()
-  
+
       toggleElementsWindowsAuth(isLogin)
     })
-    
+
     windowAuth.addEventListener('click', (event) => {
       const target = event.target
 
@@ -30,11 +30,14 @@ export const toggleWindowAuth = () => {
       if (target && target.closest('#auth-link')) {
         isLogin = false
         toggleElementsWindowsAuth(isLogin)
+
+        document.body.classList.add('_is-register')
       }
 
       if (target && target.closest('#login-link')) {
         isLogin = true
         toggleElementsWindowsAuth(isLogin)
+        document.body.classList.remove('_is-register')
       }
     })
   }
